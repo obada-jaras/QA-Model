b79ab8a99d1edaf42fd8647106653f37477bdb5c
OBADA TAHAYNA <obada.jaras@yahoo.com>
2023-05-04 04:07:29 +0300

    update merge_json_files.py

--- utils/merge_json_files.py
+++ utils/merge_json_files.py
@@ -2,12 +2,12 @@ import argparse
 import json
 
 
-def merge_json_files(output_file, file_names):
+def merge_json_files(output_file, filenames):
     '''
     Assumes json files are in SQuAD format, i.e see https://github.com/facebookresearch/DrQA#format-b
-    ''' 
+    '''
     combined_data = []
-    for fname in file_names:
+    for fname in filenames:
         with open(fname, encoding="utf-8") as f:
             data = json.load(f)['data']
         for article in data:
@@ -24,11 +24,16 @@ def merge_json_files(output_file, file_names):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Merge multiple JSON files into a single file')
-    parser.add_argument('--input', '-i', nargs='+', required=True, help='Input files to merge')
+    parser.add_argument('--input', '-i', required=True, help='Input files to merge')
     parser.add_argument('--output', '-o', required=True, help='Output file path')
 
     args = parser.parse_args()
     input_files = args.input
+    
+    # Split the filenames string into a list and remove any empty strings
+    input_files = input_files.split('\n')
+    input_files = list(filter(None, input_files))
+
     output_file = args.output
 
     merge_json_files(output_file, input_files)
